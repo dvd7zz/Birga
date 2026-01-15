5e712775429f90cc2eeb22b4c490bd61d6750358
just lenx <uninlenx7@gmail.com>
2026-01-15 21:07:28 +0500

    Add functions to open and close mobile menu

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function openMenu() {
+    document.getElementById("mobileMenu").style.left = "0";
+  }
+  
+  function closeMenu() {
+    document.getElementById("mobileMenu").style.left = "-100%";
+  }
